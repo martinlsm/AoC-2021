2aa67f83a95b9d90e8309bfffbe49a2db1bbf8f5
Martin Lindström <martin.lindstrom94@gmail.com>
2021-12-15 20:30:22 +0100

    Add solution to 15

--- aoc15.py
+++ aoc15.py
@@ -1,14 +1,35 @@
 import numpy as np
 
-def find_lowest_cost(costs, visited):
+class Node():
+    def __init__(self, r, c, H, W):
+        self.r = r
+        self.c = c
+        self.h = np.abs(r - (H - 1)) + np.abs(c - (W - 1))
+        #self.h = 0
+        self.g = np.inf
+        self.prev = None
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f'Node({self.r,self.c})'
+
+    def __hash__(self):
+        return hash((self.r, self.c))
+
+    def __eq__(self, other):
+        return self.r == other.r and self.c == other.c
+
+def find_lowest_cost(open_nodes):
     lo = np.inf
     res = None
-    for i in range(costs.shape[0]):
-        for j in range(costs.shape[1]):
-            if costs[i,j] < lo and (i,j) not in visited:
-                res = (i,j)
-                lo = costs[i,j]
+    for node in open_nodes:
+        if node.g + node.h < lo:
+            lo = node.g + node.h
+            res = node
     return res
+
 
 def inside(r,c,H,W):
     return r >= 0 and c >= 0 and r < H and c < W
@@ -26,6 +47,13 @@ def find_neighbours(r,c,H,W):
     return neighbours
 
 
+def get_elem(matrix, r, c):
+    rmod = r % matrix.shape[0]
+    cmod = c % matrix.shape[1]
+    val = matrix[rmod,cmod]
+    rr = r // matrix.shape[0]
+    cc = c // matrix.shape[1]
+    return ((val - 1 + rr + cc) % 9) + 1
 
 
 with open('input15') as f:
@@ -39,29 +67,39 @@ for line in lines:
         matrix[-1].append(int(c))
 
 matrix = np.array(matrix)
-costs = np.empty(shape=matrix.shape)
-costs.fill(np.inf)
-costs[0,0] = 0
-visited = set()
 
-H = matrix.shape[0]
-W = matrix.shape[1]
+H = 5 * matrix.shape[0]
+W = 5 * matrix.shape[1]
 
-prev = [[(-1,-1) for _ in range(W)] for _ in range(H)]
+start = Node(0,0,H,W)
+start.g = 0
+nodes = {}
+nodes[(0,0)] = start
+open_nodes = set([start])
 
+end = None
 while True:
-    lowest = find_lowest_cost(costs, visited)
-    if lowest is not None:
-        (r,c) = lowest
-    else:
-        break
-    if (r,c) == (H - 1,W - 1):
-        break
-    visited.add((r,c))
+    node = find_lowest_cost(open_nodes)
+    assert node is not None
 
-    neighbours = find_neighbours(r,c,H,W)
+    if node.r == H - 1 and node.c == W - 1:
+        end = node
+        break
 
+    open_nodes.remove(node)
+
+    neighbours = find_neighbours(node.r,node.c,H,W)
     for (nr,nc) in neighbours:
-        if costs[nr,nc] > costs[r,c] + matrix[nr,nc]:
-            costs[nr,nc] = costs[r,c] + matrix[nr,nc]
-            prev[nr][nc] = (r,c)
+        if (nr,nc) not in nodes:
+            nodes[(nr,nc)] = Node(nr,nc,H,W)
+
+        edge_cost = get_elem(matrix, nr, nc)
+        tentative_cost = node.g + edge_cost
+        if tentative_cost < nodes[(nr,nc)].g:
+            nodes[(nr,nc)].g = tentative_cost
+            nodes[(nr,nc)].prev = node
+            if nodes[(nr,nc)] not in open_nodes:
+                open_nodes.add(nodes[(nr,nc)])
+
+
+print(end.g)
